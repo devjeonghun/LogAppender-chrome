96c8d3cdc8b8036b5ff75efe115670dc75983945
devjeonghun <jeonghun0204@gmail.com>
2019-02-15 10:12:58 +0900

    Initial commit

---
+++ devtools.js
@@ -0,0 +1,5 @@
+// Can use
+// chrome.devtools.*
+// chrome.extension.*
+// Create a tab in the devtools area
+chrome.devtools.panels.create("logAppender", "main.ico", "options.html", function(panel) {});
